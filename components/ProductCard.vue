93166afde269a57b72008ed3312a4b40d1b300ae
ROSEY David <david.rosey.pro@gmail.com>
2021-01-24 10:21:31 +0100

    Creating categories page

---
+++ components/ProductCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-hover v-slot="{ hover }">
+    <v-card
+      :elevation="hover ? 16 : 2"
+      class="pa-4"
+      style="cursor: pointer"
+      @click="$emit('clicked', product)"
+    >
+      <v-img
+        :src="product.productImage"
+        alt="product.productName"
+        contain
+        height="200px"
+      ></v-img>
+
+      <v-row align="center" justify="center">
+        <div
+          class="v-card__title"
+          :style="{ color: $vuetify.theme.themes.light.primary }"
+        >
+          {{ product.productName }}
+        </div>
+      </v-row>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
---
+++ components/ProductDialogue.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-dialog :value="dialog" persistent max-width="600px">
+    <v-card class="pa-6">
+      <v-img
+        :src="product.productImage"
+        alt="product.productName"
+        contain
+        max-height="400px"
+      ></v-img>
+
+      <div
+        class="v-card__title my-4"
+        :style="{ color: $vuetify.theme.themes.light.primary }"
+      >
+        {{ product.productName }}
+      </div>
+
+      <v-card-subtitle>
+        {{ product.productDescription }}
+      </v-card-subtitle>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" text @click="$emit('update:dialog', false)">
+          Close
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: {
+      type: Boolean,
+      default: false,
+    },
+    product: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
