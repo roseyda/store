7431d11ee96c067a8beb4ad86183b538424937ca
ROSEY David <david.rosey.pro@gmail.com>
2021-01-24 10:21:57 +0100

    Cleaning + default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,61 +1,36 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app>
       <v-list>
         <v-list-item
-          v-for="(item, i) in items"
+          v-for="(item, i) in navItems"
           :key="i"
           :to="item.to"
-          router
+          nuxt
           exact
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar :clipped-left="clipped" fixed app color="primary">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+      <transition name="fade" mode="out-in">
+        <v-toolbar-title :key="$route.fullPath">
+          {{ pageTitle }}
+        </v-toolbar-title>
+      </transition>
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
-        <nuxt />
+        <transition name="fade" mode="out-in">
+          <nuxt :key="$route.fullPath" />
+        </transition>
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -66,26 +41,56 @@
 export default {
   data() {
     return {
-      clipped: false,
+      clipped: true,
       drawer: false,
       fixed: false,
-      items: [
+    }
+  },
+  async fetch() {
+    await this.$store.dispatch('loadProducts')
+    await this.$store.dispatch('loadCategories')
+  },
+
+  computed: {
+    pageTitle() {
+      return (
+        this.capitalize(
+          this.$store.getters.getCategoryByCategoryId(
+            this.$route.params.categoryId
+          )?.categoryName
+        ) ?? 'Home'
+      )
+    },
+    navItems() {
+      return [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Home',
           to: '/',
         },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    }
+        ...this.$store.state.categories.map((cat) => ({
+          title: this.capitalize(cat.categoryName),
+          to: `/categories/${cat.categoryId}`,
+        })),
+      ]
+    },
+  },
+  methods: {
+    capitalize(string) {
+      return typeof string === 'string'
+        ? string.charAt(0).toUpperCase() + string.slice(1)
+        : undefined
+    },
   },
 }
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
