2ed7ad04006d91e7b69aab41be1bdd52048bf93e
ROSEY David <david.rosey.pro@gmail.com>
2021-01-24 10:21:07 +0100

    update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: false,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
@@ -42,14 +42,21 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL:
+      'https://api.sheety.co/06def408e74850aef0fbd22a79539f9f/ambanidedat',
+    https: true,
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
+        light: {
+          primary: colors.green.darken1,
+        },
         dark: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
